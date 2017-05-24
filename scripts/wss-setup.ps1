e2fa4213b3f033a174c6439703a8ff7197b76e75
Al West <al@tsew.net>
2017-05-24 16:19:21 +0100

    Fixed line ending for registry entry

--- scripts/wss-setup.ps1
+++ scripts/wss-setup.ps1
@@ -35,7 +35,7 @@ switch ( (Get-WssConfigurationStatus).Status) {
 
         LogWrite 'Adding registry key'
 
-        Set-ItemProperty -Path $RegistryKey -Name $RegistryEntry -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -File $($ScriptPath)" 
+        Set-ItemProperty -Path $RegistryKey -Name $RegistryEntry -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -File $($ScriptPath)"
 
     }
 
@@ -61,3 +61,4 @@ switch ( (Get-WssConfigurationStatus).Status) {
     }
 
 }
+
